b28da598a5840dc2131cf8fb206c33e9bbdf781f
FarhanBadrussalam <badrussalam859@gmail.com>
2022-04-23 21:23:53 +0700

    portfolio first

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: 'rgb(20 184 166)',
+        dark: '#334155',
+        secondary: '#64778b',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
